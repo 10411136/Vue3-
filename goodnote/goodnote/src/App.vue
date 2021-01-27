5750d3e557b08524a78a86bc116cab1e23ba258a
YuLin <10411136@gm.nttu.edu.tw>
2021-01-27 23:05:02 +0800

    修改檔案

--- goodnote/goodnote/src/App.vue
+++ goodnote/goodnote/src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> 
   <router-view/>
 </template>
 
@@ -17,11 +17,9 @@
 
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
